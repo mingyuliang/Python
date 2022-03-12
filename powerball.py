aa796b405aa15391e5730259e72c0dfeba2f1cda
MingYu Liang <92761806+mingyuliang@users.noreply.github.com>
2022-03-12 11:02:19 -0500

    Get your own powerball numbers.
    Compute and print powerball numbers.

---
+++ powerball.py
@@ -0,0 +1,14 @@
+"""
+Compute and print powerball numbers.
+"""
+
+import random
+
+def powerball():
+    #To create a list of random numbers without duplicates with Python,use the random.sample method.
+    r1 = random.sample(range(60), 5)
+    r2 = random.randrange(36)
+    result = "Today's numbers are {}, {}, {}, {} and {}. The powerball number is {}.".format(r1[0],r1[1],r1[2],r1[3],r1[4],r2)
+    print(result)
+
+powerball()
