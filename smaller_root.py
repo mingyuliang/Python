088d2adb9b92c916e7573180f68dee4beb2d257e
MingYu Liang <92761806+mingyuliang@users.noreply.github.com>
2022-03-18 18:22:48 -0400

    CodeSkulptor3 code

---
+++ smaller_root.py
@@ -0,0 +1,59 @@
+"""
+Solution - Compute the smaller root of a quadratic equation.
+"""
+
+###################################################
+# Smaller quadratic root formula
+# Student should enter function on the next lines.
+def smaller_root(coeff_a, coeff_b, coeff_c):
+    """
+    Returns the smaller root of a quadratic equation with the
+    given coefficients.
+    """
+
+    discriminant = coeff_b ** 2 - 4 * coeff_a * coeff_c
+    if discriminant < 0 or coeff_a == 0:
+        print("Error: No real solutions")
+    else:
+        discriminant_sqrt = discriminant ** 0.5
+        # Choose the positive or negative square root that leads to a smaller root.
+        if coeff_a > 0:
+            smaller = - discriminant_sqrt
+        else:
+            smaller = discriminant_sqrt
+        return (-coeff_b + smaller) / (2 * coeff_a)
+
+
+###################################################
+# Tests
+# Student should not change this code.
+
+coeff_a, coeff_b, coeff_c = 1, 2, 3
+print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
+      "x + " + str(coeff_c) + " is: ")
+print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
+
+coeff_a, coeff_b, coeff_c = 2, 0, -10
+print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
+      "x + " + str(coeff_c) + " is: ")
+print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
+
+
+coeff_a, coeff_b, coeff_c = 6, -3, 5
+print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
+      "x + " + str(coeff_c) + " is: ")
+print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#The smaller root of 1x^2 + 2x + 3 is:
+#Error: No real solutions
+#None
+#The smaller root of 2x^2 + 0x + -10 is:
+#-2.2360679775
+#The smaller root of 6x^2 + -3x + 5 is:
+#Error: No real solutions
+#None
