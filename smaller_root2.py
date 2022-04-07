3dc07bfd62b6a5d1d2314229f0e1b77689cb08e0
MingYu Liang <92761806+mingyuliang@users.noreply.github.com>
2022-04-07 10:42:05 -0400

    Update smaller_root2.py

--- smaller_root2.py
+++ smaller_root2.py
@@ -27,12 +27,13 @@ coeff_a, coeff_b, coeff_c = 1, 2, 3
 print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
       "x + " + str(coeff_c) + " is: ")
 print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
+print()
 
 coeff_a, coeff_b, coeff_c = 2, 0, -10
 print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
       "x + " + str(coeff_c) + " is: ")
 print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
-
+print()
 
 coeff_a, coeff_b, coeff_c = 6, -3, 5
 print("The smaller root of " + str(coeff_a) + "x^2 + " + str(coeff_b) +
@@ -46,9 +47,10 @@ print(str(smaller_root(coeff_a, coeff_b, coeff_c)))
 
 #The smaller root of 1x^2 + 2x + 3 is:
 #Error: No real solutions
-#None
+
 #The smaller root of 2x^2 + 0x + -10 is:
 #-2.2360679775
+
 #The smaller root of 6x^2 + -3x + 5 is:
 #Error: No real solutions
-#None
+
